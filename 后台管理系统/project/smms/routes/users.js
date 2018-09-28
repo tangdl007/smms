ac0c7128390e8a8d64632e7ba804076926574e50
tang <123@qq.com>
2018-09-28 22:55:46 +0800

    完成登陆和推出

--- 后台管理系统/project/smms/routes/users.js
+++ 后台管理系统/project/smms/routes/users.js
@@ -12,6 +12,8 @@ connection.connect(()=>{
 //接受前端的请求
 router.post("/userAdd",(req,res)=>{
     let{username,password,groups} = req.body;
+
+    console.log(groups.length);
     //构造sql语句
     const  sqlStr = "insert into users(username,password,groups) values(?,?,?)";
     //接受到的数据参数
@@ -60,5 +62,97 @@ router.get("/delData",(req,res)=>{
   })
 })
 
+//接受修改的请求
+
+router.get("/editUser",(req,res)=>{
+    let {id} = req.query;
+    let sqlStr = `select * from users where id = ${id}`;
+    connection.query(sqlStr,(err,data)=>{
+      if(err){
+        throw err
+      }else{
+        res.send(data)
+      }
+    })
+})
+
+//接受修改的数据
+router.post("/userEdit",(req,res)=>{
+  let{username,password,groups,id} = req.body;
+  //注意变量要用单引号引起来
+  const sqlStr = `update users set username = '${username}',password = '${password}',groups = '${groups}' where id = ${id}`
+  connection.query(sqlStr,(err,data)=>{
+    if(err){
+      throw err
+    }else{
+      if(data.affectedRows>0){
+        res.send({"errcode":1,"msg":"修改成功"})
+      }else{
+        res.send({"errcode":0,"msg":"修改失败"})
+      }
+    }
+  })
+})
+
+//接受批量删除的请求
+router.post("/batchesDel",(req,res)=>{
+  let idArr = req.body["idArr[]"]
+  
+  const sqlStr = `delete from users where id in ('${idArr}')`;
+
+  connection.query(sqlStr,(err,data)=>{
+    if(err){
+      throw err
+    }else{
+      if(data.affectedRows>0){
+        res.send({"errcode":1,"msg":"删除成功"})
+      }else{
+        res.send({"errcode":0,"msg":"删除失败"})
+      }
+    }
+  });
+})
+
+//接受检查用户名是否存在
+
+router.post("/loginCheck",(req,res)=>{
+    let {username,password} = req.body;
+    const sqlStr = `select * from users where username='${username}' and password='${password}'`;
+    connection.query(sqlStr,(err,data)=>{
+      if(err){
+        throw err;
+      }else{
+        if(data.length){
+          //如果有值那么设置cookie并响应数据
+          res.cookie('username',data[0].username);
+          res.cookie('password',data[0].password);
+          res.cookie('groups',data[0].groups);
+          res.send({"errcode":1,"msg":"登陆成功"})
+        }else{
+          res.send({"errcode":0,"msg":"请检查你的用户名和密码"})
+        }
+      }
+    })
+})
+
+//检验是否已经登陆
+router.get("/checkIsLogin",(req,res)=>{
+  let username = req.cookies.username;
+  if(username){
+    res.send('console.log("")')
+  }else{
+    //因为使用script发送的请求所以res.send自带script标签不用特别的写
+    res.send('alert("你想多了");location.href="./login.html";')
+  }
+})
+
+
+//推出登陆
+router.get("/logout",(req,res)=>{
+  res.clearCookie("username");
+  res.clearCookie("password");
+  res.clearCookie("groups");
+  res.send('<script>alert("再见");location.href="http://localhost:555/login.html";</script>')
+})
 
 module.exports = router;
